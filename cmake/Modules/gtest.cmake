0150dccabfba77e33a3f27aa06bc01f092dad143
psychobilli <richardsw1123@gmail.com>
2018-09-19 21:46:14 -0400

    Updates to CMake files.  Deactivated automated testing.

--- cmake/Modules/gtest.cmake
+++ cmake/Modules/gtest.cmake
@@ -30,9 +30,9 @@ function(add_gmock_test target)
 
     add_test(${target} ${target})
 
-    add_custom_command(TARGET ${target}
-                       POST_BUILD
-                       COMMAND ${target}
-                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                       COMMENT "Running ${target}" VERBATIM)
+#    add_custom_command(TARGET ${target}
+#                       POST_BUILD
+#                       COMMAND ${target}
+#                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+#                       COMMENT "Running ${target}" VERBATIM)
 endfunction()
