ce0ba1467aa5d4bd1aae7180f4551f34b61f9459
psychobilli <richardsw1123@gmail.com>
2018-09-23 23:01:47 -0400

    Added a unit test for a packet handling method.  Broke PlayerTest in the process.

--- src/server/gameTest/WorldSessionTest.cpp
+++ src/server/gameTest/WorldSessionTest.cpp
@@ -3,9 +3,34 @@ using ::testing::Eq;
 #include <gtest/gtest.h>
 using ::testing::Test;
 
+#include "Player.h"
+#include "CharacterHandler.cpp"
 #include "WorldServerMoq.h"
+#include "WorldSession.h"
+#include "WorldPacket.h"
 #include <boost/asio/ip/tcp.hpp>
 #include <functional>
+
+TEST(WorldSession, InitializeSession)
+{
+    int argc = 0;
+    char** argv = new char*[0];
+    WorldServerMoq* server = new WorldServerMoq();
+    server->LaunchServer(argc, argv);
+
+    uint32 id = 1;
+    std::shared_ptr<AuthSession> authSession;
+    std::shared_ptr<WorldSocket> worldSocket;
+    PreparedQueryResult result;
+    int64 mutetime = 0;
+    int8 locale = 0;
+    WorldSession* _worldSession;
+    _worldSession = new WorldSession(id, "ADMIN", worldSocket, AccountTypes(0),
+        2, mutetime, LOCALE_enUS, 0, false);
+    _worldSession->InitializeSession();
+
+    ASSERT_THAT(id, Eq(_worldSession->GetAccountId()));
+}
 
 TEST(WorldSession, CMSG_AUTH_SESSION)
 {
@@ -14,17 +39,78 @@ TEST(WorldSession, CMSG_AUTH_SESSION)
     WorldServerMoq* server = new WorldServerMoq();
     server->LaunchServer(argc, argv);
 
-    std::function<std::pair<tcp::socket*, uint32>()> _socketFactory;
-    tcp::socket* socket;
-    uint32 threadIndex;
-    std::pair<tcp::socket*, uint32> defaultFactory(std::make_pair(socket, 0));
-//    AsyncAcceptor *acceptor = main_moq::GetAcceptor();
-    //_socketFactory(std::bind(&defaultFactory, *acceptor));
-    std::tie(socket, threadIndex) = _socketFactory();
-    //WorldSocket worldSocket();
-    /*WorldSocket worldSocket();
-    sWorldSocketMgr.OnSocketOpen(worldSocket, 0);*/
-    //tcp::socket *socket = new tcp::socket();
-    //WorldSocket *worldSocket = new WorldSocket(&socket);
+    //tcp::socket&& socket();
+    //WorldSocket *worldSocket = new WorldSocket(socket);
+    std::shared_ptr<WorldSocket> worldSocket;
+    MessageBuffer mb = worldSocket->GetReadBuffer();
+
     ASSERT_EQ(0, 0);
+}
+
+TEST(WorldSession, HandleMoveTeleportAck)
+{
+    int argc = 0;
+    char** argv = new char*[0];
+    WorldServerMoq* server = new WorldServerMoq();
+    server->LaunchServer(argc, argv);
+    uint32 id = 1;
+    std::shared_ptr<AuthSession> authSession;
+    std::shared_ptr<WorldSocket> worldSocket;
+    PreparedQueryResult result;
+    int64 mutetime = 0;
+    int8 locale = 0;
+    WorldSession* _worldSession;
+    _worldSession = new WorldSession(id, "ADMIN", worldSocket, AccountTypes(0),
+        2, mutetime, LOCALE_enUS, 0, false);
+
+    Player* p = new Player(_worldSession);
+    ObjectGuid playerGuid;
+    playerGuid.Set(1);
+    LoginQueryHolder *holder = new LoginQueryHolder(id, playerGuid);
+    holder->Initialize();
+    QueryResultHolderFuture _charLoginCallback = CharacterDatabase.DelayQueryHolder(holder);
+    p->LoadFromDB(playerGuid, reinterpret_cast<LoginQueryHolder*>(_charLoginCallback.get()));
+
+    _worldSession->InitializeSession();
+    WorldPacket packet(MSG_MOVE_TELEPORT_ACK, 41);
+    packet.appendPackGUID(10);
+    packet << uint32(0);
+    packet << uint32(1);
+    WorldPacket *packetRef = &packet;
+
+    _worldSession->HandleMoveTeleportAck(*packetRef);
+}
+
+TEST(WorldSession, HandleMoveWorldportAckCode)
+{
+    int argc = 0;
+    char** argv = new char*[0];
+    WorldServerMoq* server = new WorldServerMoq();
+    server->LaunchServer(argc, argv);
+    uint32 id = 1;
+    std::shared_ptr<AuthSession> authSession;
+    std::shared_ptr<WorldSocket> worldSocket;
+    PreparedQueryResult result;
+    int64 mutetime = 0;
+    int8 locale = 0;
+    WorldSession* _worldSession;
+    _worldSession = new WorldSession(id, "ADMIN", worldSocket, AccountTypes(0),
+        2, mutetime, LOCALE_enUS, 0, false);
+
+    Player* p = new Player(_worldSession);
+    ObjectGuid playerGuid;
+    playerGuid.Set(1);
+    LoginQueryHolder *holder = new LoginQueryHolder(id, playerGuid);
+    holder->Initialize();
+    QueryResultHolderFuture _charLoginCallback = CharacterDatabase.DelayQueryHolder(holder);
+    p->LoadFromDB(playerGuid, reinterpret_cast<LoginQueryHolder*>(_charLoginCallback.get()));
+
+    _worldSession->InitializeSession();
+    WorldPacket packet(MSG_MOVE_WORLDPORT_ACK, 41);
+    packet.appendPackGUID(10);
+    packet << uint32(0);
+    packet << uint32(1);
+    WorldPacket *packetRef = &packet;
+
+    _worldSession->HandleMoveWorldportAckOpcode(*packetRef);
 }
--- src/server/gameTest/gameTest.cpp
+++ src/server/gameTest/gameTest.cpp
@@ -7,8 +7,6 @@
 #include "Config.h"
 #include "DatabaseEnv.h"
 #include "DatabaseLoader.h"
-#include "Warden.h"
-#include "WardenWin.h"
 #include "WorldSocket.h"
 #include "WorldSession.h"
 #include <boost/asio/signal_set.hpp>
@@ -38,30 +36,4 @@ TEST(DatabaseLoader, AddDatabaseConfigFile)
     loader
         .AddDatabase(CharacterDatabase, "Character");
     ASSERT_TRUE(loader.Load());
-}
-
-TEST(WorldSession, InitializeSession)
-{
-    int argc = 0;
-    char** argv;
-    auto configFile = fs::absolute("E:/Programs/TrinityWrathMod/worldserver.conf.test");
-    std::string configError;
-    sConfigMgr->LoadInitial(configFile.generic_string(), std::vector<std::string>(argv, argv + argc), configError);
-    DatabaseLoader loader("server.worldserver", DatabaseLoader::DATABASE_NONE);
-    loader
-        .AddDatabase(CharacterDatabase, "Character");
-    ASSERT_TRUE(loader.Load());
-
-    uint32 id = 1;
-    std::shared_ptr<AuthSession> authSession;
-    std::shared_ptr<WorldSocket> worldSocket;
-    PreparedQueryResult result;
-    int64 mutetime = 0;
-    int8 locale = 0;
-    WorldSession* _worldSession;
-    _worldSession = new WorldSession(id, "ADMIN", worldSocket, AccountTypes(0),
-        2, mutetime, LOCALE_enUS, 0, false);
-    _worldSession->InitializeSession();
-
-    ASSERT_THAT(id, Eq(_worldSession->GetAccountId()));
 }
