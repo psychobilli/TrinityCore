3d07469e3af1bbd071c5cfff8d6ac5b1a98f2c19
psychobilli <richardsw1123@gmail.com>
2021-01-06 09:34:58 -0500

    Removed duplicate creatures from Hodir quests, changed item level requirements for dungeons.

---
+++ sql/updates/world/3.3.5/2021_01_06_00_Dungeon_iLevel_Req.sql
@@ -0,0 +1,5 @@
+UPDATE `quest_template` SET `item_level` = 170 WHERE `mapId` IN (574,575,576,578,595,599,600,601,602,604,608,619);
+UPDATE `quest_template` SET `item_level` = 185 WHERE `level_min` = 75 AND `mapId` IN (650);
+UPDATE `quest_template` SET `item_level` = 200 WHERE `level_min` = 80 AND `mapId` IN (650);
+UPDATE `quest_template` SET `item_level` = 200 WHERE `level_min` = 75 AND `mapId` IN (632,658,668);
+UPDATE `quest_template` SET `item_level` = 215 WHERE `level_min` = 80 AND `mapId` IN (632,658,668);
